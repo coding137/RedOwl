8eb89cbd6355d505025e6e8dafe1a13a96ca5ec9
minkyu kim <mj110602@gmail.com>
2017-02-24 18:21:03 +0900

    login.php푸쉬

---
+++ login.php
@@ -0,0 +1,31 @@
+<?php 
+ define('HOST','192.168.43.234');
+ define('USER','root');
+ define('PASS','123456789');
+ define('DB','redowl');
+ $con = mysqli_connect(HOST,USER,PASS,DB) or die('Unable to Connect');
+
+ $email = $_POST['email'];
+ $pass = $_POST['pass'];
+ 
+ //Creating sql query
+ $sql = "SELECT * FROM members WHERE email='$email' AND pass='$pass'";
+ 
+ //importing dbConnect.php script 
+ 
+ //executing query
+ $result = mysqli_query($con,$sql);
+ 
+ //fetching result
+ $check = mysqli_fetch_array($result);
+ 
+ //if we got some result 
+ if(isset($check)){
+ //displaying success 
+ echo "success";
+ }else{
+ //displaying failure
+ echo "failure";
+ }
+ mysqli_close($con);
+  ?>
