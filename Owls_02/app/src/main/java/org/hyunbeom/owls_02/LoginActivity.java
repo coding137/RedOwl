6b3635347af048c7a418f56e2373777f928b35aa
minkyu kim <mj110602@gmail.com>
2017-02-24 18:16:53 +0900

    브랜치 합본

--- Owls_02/app/src/main/java/org/hyunbeom/owls_02/LoginActivity.java
+++ Owls_02/app/src/main/java/org/hyunbeom/owls_02/LoginActivity.java
@@ -1,18 +1,59 @@
 package org.hyunbeom.owls_02;
-
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.app.Dialog;
+import  android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
 
-public class LoginActivity extends AppCompatActivity {
+import java.util.HashMap;
+import java.util.Map;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.List;
+public class LoginActivity extends AppCompatActivity{
+
+    private boolean loggedIn = false;
+
+    private EditText editTextEmail;
+    private EditText editTextPass;
+
+    public static final String EMAIL = "email";
+
+    String email;
+    String pass;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        editTextEmail = (EditText) findViewById(R.id.email);
+        editTextPass = (EditText) findViewById(R.id.pass);
 
         TextView registerButton = (TextView) findViewById(R.id.signupButton);
         registerButton.setOnClickListener(new View.OnClickListener() {
@@ -21,17 +62,115 @@ public class LoginActivity extends AppCompatActivity {
                 Intent registerIntent = new Intent(LoginActivity.this, RegisterActivity.class);
                 LoginActivity.this.startActivity(registerIntent);
             }
-        });
+        });//instant end
 
         Button loginButton = (Button) findViewById(R.id.loginButton);
-        
-        loginButton.setOnClickListener(new View.OnClickListener(){
+
+        //  loginButton.setOnClickListener(new View.OnClickListener() {
+
+          /*  @Override
+            public void onClick(View view) {
+            //    invokeLogin(view);
+                //Intent mainintent = new Intent(LoginActivity.this, MainActivity.class);
+                //LoginActivity.this.startActivity(mainintent);
+            }
+        });//instant end
+        */
+    }// onCreate end
+
+    public void invokeLogin(View view){
+        email = editTextEmail.getText().toString();
+        pass = editTextPass.getText().toString();
+
+        login(email,pass);
+
+    }
+
+    private  void login(final  String email, String  pass){
+
+
+        class  LoginAsync extends AsyncTask<String, Void, String>{
+
+            Dialog loadingDialog;
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+                loadingDialog = ProgressDialog.show(LoginActivity.this, "Please wait", "Loading...");
+            }//on PreExecute end in login async
+
 
             @Override
-            public void onClick(View view) {
-                Intent mainintent = new Intent(LoginActivity.this, MainActivity.class);
-                LoginActivity.this.startActivity(mainintent);
-            }
-        });
+            protected String doInBackground(String... params) {
+                String email = params[0];
+                String pass = params[1];
+
+                InputStream is = null;
+                List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
+                nameValuePairs.add(new BasicNameValuePair("email", email));
+                nameValuePairs.add(new BasicNameValuePair("pass", pass));
+                String result = null;
+
+                try{
+                    HttpClient httpClient = new DefaultHttpClient();
+                    HttpPost httpPost = new HttpPost(
+                            "http://192.168.43.234/login.php");
+                    httpPost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+                    HttpResponse response = httpClient.execute(httpPost);
+                    HttpEntity entity = response.getEntity();
+                    is = entity.getContent();
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(is, "UTF-8"), 8);
+                    StringBuilder sb = new StringBuilder();
+
+                    String line = null;
+                    while ((line = reader.readLine()) != null)
+                    {
+                        sb.append(line + "\n");
+                    }
+                    result = sb.toString();
+                } catch (ClientProtocolException e) {
+                    e.printStackTrace();
+                } catch (UnsupportedEncodingException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                return result;
+            }//
+            @Override
+            protected void onPostExecute(String result) {
+
+                String s = result.trim();
+                loadingDialog.dismiss();
+                if(s.equalsIgnoreCase("success")){
+                    Intent intent = new Intent(LoginActivity.this, ProfileActivity.class);
+                    intent.putExtra(EMAIL, email);
+                    finish();
+                    startActivity(intent);
+                }else {
+                    Toast.makeText(getApplicationContext(), "Invalid User Name or Password", Toast.LENGTH_LONG).show();
+                }
+            }//end
+
+        }// Login Async innerclass end
+
+        LoginAsync la = new LoginAsync();
+        la.execute(email, pass);
+    }//login end
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        //    getMenuInflater().inflate(R.);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.signupButton) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
